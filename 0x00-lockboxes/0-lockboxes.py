ad975d8bf3c5ae78e214afbc92622e8af281a2fb
jpenariasholberton <1875@holbertonschool.com>
2021-04-21 12:27:17 -0500

    fix pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 
 Write a method that determines if all the boxes can be opened.
 
@@ -13,6 +14,7 @@ The first box boxes[0] is unlocked
 Return True if all boxes can be opened, else return False
 
 """
+
 
 def canUnlockAll(boxes):
     keys = set()
