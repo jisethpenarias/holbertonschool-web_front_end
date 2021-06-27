0c608864d11d0059b7721752bdb6208fe0a851c4
jpenariasholberton <1875@holbertonschool.com>
2021-06-26 22:10:52 -0500

    fix

--- 0x0C-Javascript_advanced/5-mode.js
+++ 0x0C-Javascript_advanced/5-mode.js
@@ -11,7 +11,6 @@ function changeMode(size, weight, transform, background, color) {
 let spooky = changeMode(9, "bold", "uppercase", "pink", "green");
 let darkMode = changeMode(12, "bold", "capitalize", "black", "white");
 let screamMode = changeMode(12, "normal", "lowercase", "white", "black");
-    
 
 function main() {
         
